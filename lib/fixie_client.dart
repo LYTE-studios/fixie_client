9fd8bc571d467664d4306aa34eb30e93ef1e72b7
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-06-18 19:25:57 +0200

    Update fixie_client.dart

--- lib/fixie_client.dart
+++ lib/fixie_client.dart
@@ -1,2 +1,4 @@
 export 'src/protocol/protocol.dart';
 export 'package:serverpod_client/serverpod_client.dart';
+export 'package:serverpod_auth_client/src/protocol/user_info.dart';
+export 'package:serverpod_auth_client/src/protocol/authentication_response.dart';
